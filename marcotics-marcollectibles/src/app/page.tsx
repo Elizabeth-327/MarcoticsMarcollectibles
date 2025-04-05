bc81b28b89742c3dd820cfd9b63dda7b19bbdccb
hwolong <130803554+hwolong@users.noreply.github.com>
2025-04-05 14:25:27 -0500

    imported dependencies, react-leaflet tests

--- marcotics-marcollectibles/src/app/page.tsx
+++ marcotics-marcollectibles/src/app/page.tsx
@@ -1,7 +1,34 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import 'leaflet/dist/leaflet.css'
+import React, { useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+
 import jsonData from "./output.json" assert { type: "json" };
 console.log(jsonData);
+
+
+const SimpleMap = () => {
+  const mapRef = useRef(null);
+  const latitude = 51.505;
+  const longitude = -0.09;
+
+  return ( 
+    // Make sure you set the height and width of the map container otherwise the map won't show
+      <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* Additional map layers or components can be added here */}
+      </MapContainer>
+  );
+};
+
+
+
 
 type resultData = {
   name: string,
@@ -46,23 +73,29 @@ export function JsonList() {
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <label>
-          Your first name:<br></br>
-          <input name="searchquery" /> 
-          <button>Search</button>
-        </label>
-        <JsonList />
-      </main>
+    <div>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+        crossOrigin=""/>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <SimpleMap />
+          {/* <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          /> */}
+          {/* <label>
+            Your first name:<br></br>
+            <input name="searchquery" /> 
+            <button>Search</button>
+          </label> */}
+          {/* <JsonList /> */}
+        </main>
+      </div>
     </div>
     // <div className={styles.page}>
     //   <main className={styles.main}>
